3af26aeed709e6eef73bc12ed8842f0cbc86a07d
Qaiser Hussain <hussaqai@corp.contoursoftware.net>
2023-02-01 18:17:15 +0500

    add some functionalities

--- src/App.jsx
+++ src/App.jsx
@@ -1,47 +1,86 @@
 import './App.css';
-import { MdSend, MdDelete, MdUpdate, MdEdit } from 'react-icons/md'
-import { useEffect, useState } from 'react';
+import { useState } from 'react';
+import Incomplete from './components/Incomplete';
+import { MdKeyboardReturn } from 'react-icons/md';
 function App() {
-  const [task, setTask] = useState("");
-  const [items, setItems] = useState(['asd']);
+  const [task, setTask] = useState({ title: '', status: 'incomplete' });
+  const [incomplete, setIncomplete] = useState([]);
+  const [complete, setComplete] = useState([]);
+
   const handleSubmit = (e) => {
     e.preventDefault();
-    setItems([...items, task])
+    setIncomplete([...incomplete, { id: Math.random(), title: task.title, status: "incomplete" }]);
+    setTask({ title: '' });
   }
+
   const handleEdit = (i) => {
     console.log(i);
-
   }
-  const handleUpdate = (i) => { 
+
+  const handleUpdate = (i) => {
     console.log(i);
-
   }
+  
   const handleDelete = (i) => {
     console.log(i);
-   
-   }
+  }
+
+  const handleCheckBox = (id) => {
+    let temp = [...incomplete];
+
+    const incompleteList = temp.filter((item) => item.id !== id)
+    // const completedItem = temp.filter((item) => item.id === id)
+    temp.filter((item) => {
+      if (item.id === id) {
+        return setComplete([...complete, { id: item.id, title: item.title, status: "Completed" }])
+      }
+    })
+    setIncomplete(incompleteList);
+    // setComplete([...complete, ...completedItem])
+  }
+
+  const handleReturn = (id) => {
+    let temp = [...complete];
+    const completeList = temp.filter((item) => item.id !== id);
+    const result = temp.filter((item) => item.id === id);
+    setComplete(completeList);
+    setIncomplete([...incomplete, ...result])
+  }
+
   return (
     <div className="app">
       <div className="todo__container">
         <h1 className='todo__heading'>Todo App</h1>
         <form className="input__container" onSubmit={handleSubmit}>
-          <input type="text" placeholder='add todo...' className='todo__input' value={task} onChange={(e) => setTask(e.target.value)} />
+          <input type="text" placeholder='add todo...' className='todo__input' value={task.title} onChange={(e) => setTask({ title: e.target.value })} />
           <input className='search__btn' type="submit" value={"submit"} />
         </form>
         <div className="tasks">
-          {!items[0] ?
-            <h2>currently no task..</h2>
+          <div className="incomplete">
+            {!incomplete[0] ?
+              <p>currently no task available..</p>
+              :
+              incomplete.map((item, index) => (
+                <Incomplete key={item.id} item={item} btns={{ handleCheckBox, handleEdit, handleDelete, handleUpdate }} />
+              ))
+            }
+          </div>
+          {complete[0] ?
+            <div className='complete'>
+              {
+                complete.map((item, index) => (
+                  <div className="task" key={index}>
+                    {item.title}
+                    <div className='icons'>
+                      <MdKeyboardReturn onClick={() => handleReturn(item.id)} style={{ cursor: 'pointer', color: 'orange' }} />
+                      <div className="status">{item.status}</div>
+                    </div>
+                  </div>
+                ))
+              }
+            </div>
             :
-            items.map((item, index) => (
-              <div className="task" key={index}>
-                {item}
-                <div className='icons'>
-                 <span onClick={()=>{handleEdit(index)}}><MdEdit /></span>
-                 <span onClick={()=>{handleUpdate(index)}}><MdUpdate /></span>
-                 <span onClick={()=>{handleDelete(index)}}><MdDelete /></span>
-                </div>
-              </div>
-            ))
+            null
           }
 
         </div>
---
+++ src/components/Incomplete.jsx
@@ -0,0 +1,27 @@
+import React from 'react'
+import { MdSend, MdDelete, MdUpdate, MdEdit } from 'react-icons/md'
+
+function Incomplete({item,index,btns}) {
+    const [check, setCheck] = React.useState(false);
+    const {id,title} = item;
+
+    const handleChange = () => {
+        setCheck(prev => !prev);
+        btns.handleCheckBox(id)
+        console.log(check)
+    }
+    return (
+        <div className="task">
+            {title}
+            {check}
+            <div className='icons'>
+                <input type="checkbox" checked={check} onChange={handleChange} />
+                <span className='icon' onClick={() => { btns.handleEdit(id) }}><MdEdit /></span>
+                <span className='icon' onClick={() => { btns.handleUpdate(id) }}><MdUpdate /></span>
+                <span className='icon' onClick={() => { btns.handleDelete(id) }}><MdDelete /></span>
+            </div>
+        </div>
+    )
+}
+
+export default Incomplete;
